4df99ebe73fb4b4465541e6194f00c04f19a1dfa
Yolanda Gunter <yemgunter@gmail.com>
2024-12-04 02:23:03 -0600

    Update date.sql

--- date.sql
+++ date.sql
@@ -5,7 +5,7 @@ DECLARE
 
 BEGIN
 
-    DBMS_OUTPUT.PUT_LINE('This is my first PLSQL GIT file');
+    DBMS_OUTPUT.PUT_LINE('This is my test PLSQL GIT file');
     DBMS_OUTPUT.PUT_LINE('Today''s date is:' || today);
 
 
